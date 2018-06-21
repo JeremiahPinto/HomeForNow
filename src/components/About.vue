5cc147c5d021ef659df5e5bdd609d094e17dc756
Jeremiah Pinto <jeremiahpinto@Jeremiahs-MacBook-Air.local>
2018-06-22 07:24:09 +0800

    Added required images and a few more pages

---
+++ src/components/About.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="about">
+    <b-container>
+
+      <b-row>
+        <b-col>
+          <h1 class="bang sec-color">
+            Home For Now
+          </h1>
+          <h5 class="bold">
+            A Web App that allows young people to search for vacant places 
+            in the Crisis and Transitional Accommodation sectors, and allows
+            Services to manage requests for housing.
+          </h5>
+          <hr>
+        </b-col>      
+      </b-row>
+
+      <b-row>
+        <b-col cols="12" md="6">
+          <h2 class="bang">
+            About Us
+          </h2>
+          <p>
+            About half of the 100,000 people who are homeless on any night in 
+            Australia are under 25. The experience of homelessness is 
+            confusing and isolating. We think that the way our system works 
+            shouldn’t make that feeling worse. This web application was 
+            developed to solve one small part of the problem – the search for 
+            a vacancy in an accommodation service: a HomeForNow.
+          </p>
+        </b-col>
+        <b-col cols="12" md="6">
+          <h2 class="bang">
+            For You
+          </h2>
+          <p>
+            HomeForNow works by making a listing of the live vacancies 
+            possible and connecting young people experiencing homelessness 
+            with the services who are best placed to provide support and 
+            warmth. Above all, it puts live information directly in the hands 
+            of young people and allows them to make their own choices in 
+            their journey out of homelessness.
+          </p>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <h2 class="bang">
+            Who We Are
+          </h2>
+          <p>
+            HomeForNow was developed by a partnership between Anglicare WA, 
+            Coders for Causes and UWA’s Innovation Quarter. The tough work 
+            of development was taken on by a group of volunteers from Coders 
+            for Causes alongside emerging designers who volunteered from the 
+            community. These volunteers worked closely with Anglicare WA 
+            so that HomeForNow would closely meet the needs of young people 
+            and the sector. The team continues to oversee HomeForNow’s 
+            development, while Anglicare WA works to integrate the 
+            technology into the youth accommodation sector.
+          </p>
+        </b-col>
+      </b-row>
+
+      <b-row no-gutters="true" align-v="center">
+        <b-col cols="12" sm="6" lg="3">
+          <b-img src="../assets/CFC_Logo.png" fluid class="logo"/>
+        </b-col>
+
+        <b-col cols="12" sm="6" lg="3">
+          <b-img src="../assets/Luke_Logo.png" fluid class="logo"/>
+        </b-col>
+        
+        <b-col cols="12" sm="6" lg="3">
+          <b-img src="../assets/AWA_Horizontal.png" fluid class="logo"/>
+        </b-col>
+
+        <b-col cols="12" sm="6" lg="3">
+          <b-img src="../assets/UWA_Horizontal.png" fluid class="logo"/>
+        </b-col>
+      </b-row>
+
+      <b-row style="margin-top: 16px;">
+        <b-col>
+          <h3 class="bold">
+            Entry Point
+          </h3>
+          <h5 class="bold sec-color">
+            6496 0001 <br>
+            1800 124 684
+          </h5>
+        </b-col>
+
+        <b-col>
+          <h3 class="bold">
+            Crisis Care
+          </h3>
+          <h5 class="bold sec-color">
+            9223 1111 <br>
+            1800 199 008
+          </h5>
+        </b-col>
+      </b-row>
+
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about', // this is the name of the component
+};
+</script>
+<style>
+hr {
+  height: 2px;
+  width: 52.3437vw;
+  background-color: #616161;
+  margin-top: 0px;
+  float: left;
+}
+h2 {
+  margin-top: 20px;
+}
+.logo {
+  padding: 10px;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="index">
+    <b-container>
+
+      <b-row :style="{ marginBottom: '17vh' }">
+        <b-col cols="12" md="8">
+          <b-img src="../assets/landing_main.png" fluid/>
+        </b-col>      
+      </b-row>
+
+      <b-form @submit="event.preventDefault()" id="locationForm">
+        <b-form-group :invalid-feedback="invalidFeedback"
+                      :valid-feedback="validFeedback"
+                      :state="state"
+        >
+          <b-row>
+            <b-col cols="12" md="8">
+              <b-form-input id="location"
+                            ref="autocomplete"
+                            v-model.trim="name"
+                            size="lg"
+                            type="text"
+                            name="location"
+                            placeholder="Where are you now - e.g. Subiaco"
+                            autocomplete="on"
+                            required
+              >
+              </b-form-input>
+
+              <b-row>
+                <b-col sm="8" md="8">
+                  <p :style="{ color: '#999', marginTop: '3px', paddingLeft: '18px' }">
+                    Use <a href="#" :style="{ color: '#999' }"> my current location </a>
+                  </p>
+                </b-col>
+              </b-row>
+
+            </b-col>
+            <b-col cols="6" md="2">
+              <b-button type="submit" size="lg" variant="pink" :style="{ width: 'inherit', height: '50px' }">Right Now</b-button>
+            </b-col>
+            <b-col cols="6" md="2">
+              <b-button type="submit" size="lg" variant="outline-pink" :style="{ width: 'inherit', height: '50px' }">Long Term</b-button>
+            </b-col>
+          </b-row>
+          
+        </b-form-group>
+      </b-form>
+
+      <b-row :style="{ marginTop: '160px' }">
+        <b-col>
+          <b-img src="../assets/landing_where.png" :style="{ height: '120px' }" fluid/>
+        </b-col>
+      </b-row>
+
+      <b-row :style="{ marginTop: '100px' }">
+        <b-col>
+          <h2 class="bang" :style="{ margin: '0' }"> Close To You. </h2>
+          <h3> Nearest crisis accommodation services. </h3>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col cols="12" md="6">
+
+        </b-col>
+        <b-col cols="12" md="6">
+          
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <h2 class="bang" :style="{ margin: '0' }"> Who To Call. </h2>
+          <h3 :style="{ marginBottom: '32px' }"> Numbers to call if you need help. </h3>
+        </b-col>
+      </b-row>
+
+      <b-row :style="{ marginTop: '16px', marginBottom: '32px' }">
+        <b-col>
+          <h3 class="bold">
+            Entry Point
+          </h3>
+          <h5 class="bold sec-color">
+            6496 0001 <br>
+            1800 124 684
+          </h5>
+        </b-col>
+
+        <b-col>
+          <h3 class="bold">
+            Crisis Care
+          </h3>
+          <h5 class="bold sec-color">
+            9223 1111 <br>
+            1800 199 008
+          </h5>
+        </b-col>
+      </b-row>
+
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  // mounted() {
+  //   this.autocomplete = new google.maps.places.Autocomplete(
+  //     (this.$refs.autocomplete),
+  //     {
+  //       types: ['(cities)'],
+  //       componentRestrictions: { country: 'au' },
+  //     },
+  //   );
+    // this.autocomplete.addListener('place_changed', () => {
+    //   let place = this.autocomplete.getPlace();
+    //   let ac = place.address_components;
+    //   let lat = place.geometry.location.lat();
+    //   let lon = place.geometry.location.lng();
+    // });
+// },
+  name: 'index',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#location {
+  border-radius: 3px;
+  box-shadow: 0px 2px 7.8px 0.2px rgba(0, 0, 0, 0.3);
+  background-color: #ffffff;
+  border: solid 2px #333333;
+}
+</style>
