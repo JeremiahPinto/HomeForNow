7cf74203a573fc16101aa7b98dda99dffa191d57
fezproof <stevebob1997@hotmail.com>
2018-07-04 16:12:37 +0800

    Fixed footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,11 +1,19 @@
 <template>
-  <b-navbar id="footer" position="bottom" type="light" variant="white" toggleable="false">
-    <b-navbar-nav>
-      <b-nav-item to="about">About Us</b-nav-item>
-      <b-nav-item to="terms_of_use">Terms of Use</b-nav-item>
-      <b-nav-item to="contact">Contact Us</b-nav-item>
-    </b-navbar-nav>
-    <b-nav-text>HomeForNow &copy; 2018</b-nav-text>
+  <b-navbar id="footer" type="light" variant="white" toggleable>
+    <b-container class="justify-content-center">
+      <b-row>
+        <b-col cols="12">
+          <b-navbar-nav class="justify-content-center">
+            <b-nav-item to="about">About Us</b-nav-item>
+            <b-nav-item to="terms_of_use">Terms of Use</b-nav-item>
+            <b-nav-item to="contact">Contact Us</b-nav-item>
+          </b-navbar-nav>
+        </b-col>
+        <b-col cols="12" class="text-center">
+          <b-nav-text >HomeForNow &copy; 2018</b-nav-text>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-navbar>
 </template>
 
@@ -14,10 +22,3 @@ export default {
   methods: {},
 };
 </script>
-
-<style scoped>
-
-#footer {
-  height: 100px;
-}
-</style>
