ad4483f04e28811f53c37e6dbc2255c5d220e6ba
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-07-05 22:11:15 +0800

    Removed navbar collapse

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,25 +1,26 @@
 <template>
   <b-navbar id="navbar" fixed="top" toggleable="md" type="light" variant="white">
     <b-navbar-brand to="/">
-      <img src="../assets/logo.svg" class="d-inline-block align-top" :style="{ width: '17px' }">
+      <img src="../assets/logo.svg" class="d-inline-block align-top" :style="{ width: '16px' }">
       HomeForNow
     </b-navbar-brand>
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-collapse is-nav id="nav_collapse">
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item v-b-modal.loginmodal>Service Login</b-nav-item>
+      <login/>
+    </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <!-- <b-navbar-nav v-if="user" class="ml-auto">
+    <!-- <b-collapse is-nav id="nav_collapse">
+
+      Right aligned nav items
+      <b-navbar-nav v-if="user" class="ml-auto">
         <b-nav-item href="#">{{ welcome }}</b-nav-item>
         <b-nav-item href="#">Logout</b-nav-item>
       </b-navbar-nav>
-      <b-navbar-nav v-else class="ml-auto"> -->
-      <b-navbar-nav class="ml-auto"> <!-- usethis for now until user is defined -->
-        <b-nav-item v-b-modal.loginmodal>Service Login</b-nav-item>
-        <login/>
-      </b-navbar-nav>
+      <b-navbar-nav v-else class="ml-auto">
+      
 
-    </b-collapse>
+    </b-collapse> -->
   </b-navbar>
 </template>
 
