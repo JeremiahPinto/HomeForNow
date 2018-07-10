453ef7a9dc837b78db653d5606aab7588fe1e41d
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2018-07-10 13:22:13 +0800

    Made contact helpine a separate component

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -38,33 +38,17 @@
       </b-col>
     </b-row>
 
-    <b-row :style="{ marginTop: '16px' }">
-      <b-col>
-        <h3 class="bold">
-          Entry Point
-        </h3>
-        <h5 class="bold sec-color">
-          6496 0001<br>
-          1800 124 684
-        </h5>
-      </b-col>
-
-      <b-col>
-        <h3 class="bold">
-          Crisis Care
-        </h3>
-        <h5 class="bold sec-color">
-          9223 1111<br>
-          1800 199 008
-        </h5>
-      </b-col>
-    </b-row>
-
+    <contact/>
   </b-container>
 </template>
 
 <script>
+import ContactInfo from './Modules/ContactInfo';
+
 export default {
+  components: {
+    contact: ContactInfo,
+  },
 };
 </script>
 
---
+++ client/src/components/Modules/ContactInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container class="p-0">
+    <b-row class="mt-2 mb-4">
+      <b-col>
+        <h3 class="bold">
+          Entry Point
+        </h3>
+        <h5 class="bold sec-color">
+          6496 0001 <br>
+          1800 124 684
+        </h5>
+      </b-col>
+
+      <b-col>
+        <h3 class="bold">
+          Crisis Care
+        </h3>
+        <h5 class="bold sec-color">
+          9223 1111 <br>
+          1800 199 008
+        </h5>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
